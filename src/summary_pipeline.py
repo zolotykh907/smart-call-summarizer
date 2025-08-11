03bc1879504be037d6eac1c9c54b903f269e5d1e
zolotykh907 <i.zolotykh@g.nsu.ru>
2025-08-11 20:45:23 +0700

    fix pipeline run

--- src/summary_pipeline.py
+++ src/summary_pipeline.py
@@ -13,8 +13,11 @@ class SummaryPipeline:
         self.speaker_identifier = SpeakerIdentifier()
         self.speech_recognizer = SpeechRecognizer()
 
+    
+    def _merge_diarization_and_recognition(self, diarization_segments, recognition_segments, eps=0.1):
+        diarization_segments = sorted(diarization_segments, key=lambda x: x['start'])
+        recognition_segments = sorted(recognition_segments, key=lambda x: x['start'])
 
-    def merge_diarization_and_text(self, diarization_segments, recognition_segments, eps=0.1):
         merged_segments = []
         for segment in recognition_segments:
             recognition_start, recognition_end = segment['start'], segment['end']
@@ -24,19 +27,17 @@ class SummaryPipeline:
                 diarization_start, diarization_end = diarization_segment['start'], diarization_segment['end']
                 speaker = diarization_segment['speaker']
 
-                if (diarization_start > recognition_start - eps) and (diarization_end < recognition_end + eps):
+                if (recognition_end >= diarization_start - eps) and (recognition_start <= diarization_end + eps):
                     merged_segments.append({'speaker': speaker, 
                                 'text': text,
                                 'start': max(recognition_start, diarization_start),
                                 'end': min(recognition_end, diarization_end)})
-
+                    break
         return merged_segments
-
 
     def summary_to_markdown(self, text, filename='data/4_summary.md'):
         with open(filename, 'w', encoding='utf-8') as f:
             f.write(text)
-
 
     def dialogue_to_markdown(self, dialogue, filepath="data/4_dialogue.md"):
         def format_time(seconds):
@@ -55,8 +56,6 @@ class SummaryPipeline:
         with open(filepath, "w", encoding="utf-8") as f:
             f.write("\n".join(lines))
 
-
-
     def run(self, audio_file):
         diarization = self.speaker_identifier.identify_speakers(audio_file)
         segments_info = self.speaker_identifier.get_segments_info(diarization)
@@ -64,19 +63,17 @@ class SummaryPipeline:
         recognition_result = self.speech_recognizer.speech_to_text(audio_file)
 
         summary = self.summarizer.full_summarize(recognition_result['text'])
-        dialogue_segments = self.merge_diarization_and_text(segments_info, recognition_result['segments'])
+        dialogue_segments = self._merge_diarization_and_recognition(segments_info, recognition_result['segments'])
 
         return {'summary':summary, 
                 'dialogue':dialogue_segments}
-        
-
 
 
 if __name__ == "__main__":
     pipeline = SummaryPipeline()
-    res = pipeline.run('data/4.wav')
+    res = pipeline.run('data/5.wav')
 
-    pipeline.summary_to_markdown(res['summary'])
+    #pipeline.summary_to_markdown(res['summary'])
     pipeline.dialogue_to_markdown(res['dialogue'])
     # print(res['summary'])
     # print('\n')
